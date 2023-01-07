a3d546b744a648c46cc3aab66691791ae4b6ad67
Marco Dierenfeldt <marco.dierenfeldt@ausy-technologies.de>
2023-01-07 08:40:26 +0100

    prep for next step done

---
+++ src/components/TodoList.component.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div className="box">
+                TODO-LIST COMPONENT
+            </div>
+        )
+    }
+}
+
+export default TodoList;
