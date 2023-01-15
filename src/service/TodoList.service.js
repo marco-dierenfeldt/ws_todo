9ae5f50135d0e80e01e77b114a0e0f1b09105984
Marco Dierenfeldt <marco.dierenfeldt@ausy-technologies.de>
2023-01-15 12:13:01 +0100

    add service

---
+++ src/service/TodoList.service.js
@@ -0,0 +1,19 @@
+class TodoListService {
+    todoList = {title:'Programmieren lernen', todos:[
+            {text:'IDE installieren',done:true},
+            {text:'Node installieren',done:true},
+            {text:'Start Quellcode kopieren',done:true},
+            {text:'Erste Komponente einbinden',done:true},
+            {text:'Komponente ändern',done:true},
+            {text:'Arbeiten mit state',done:true},
+            {text:'Arbeiten mit service',done:true},
+            {text:'Kommunikation mit Properties',done:false}
+        ]
+    };
+
+    getTodoList() {
+        return this.todoList;
+    }
+}
+
+export default TodoListService();
